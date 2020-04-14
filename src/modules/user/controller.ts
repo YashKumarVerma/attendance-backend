03f8fe20870c8ed1cb03ea8017fa250cc84395c4
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-04-15 02:44:43 +0530

    user crud operations complete

--- src/modules/user/controller.ts
+++ src/modules/user/controller.ts
@@ -5,7 +5,6 @@ import { Request, Response } from 'express'
 
 class UserOperations {
   static add(req: Request) {
-    console.log(req.body)
     return new Promise<SuccessResponse>((resolve, reject) => {
       try {
         UserSchema.create(req.body.user)
@@ -52,33 +51,115 @@ class UserOperations {
     })
   }
 
-  static delete(req: Express.Request) {
+  static delete(req: Request) {
     return new Promise<SuccessResponse>((resolve, reject) => {
-      resolve({
-        message: 'some fancy resolve message',
-        error: false,
-        payload: 'some data',
-      })
+      if (!req.body.username) {
+        reject({
+          error: true,
+          message: 'username required to remove user',
+          payload: {},
+        })
+      }
+
+      const username = req.body.username
+      try {
+        UserSchema.deleteOne({ username: username })
+          .then((resp) => {
+            if (resp.deletedCount === 1) {
+              resolve({
+                error: false,
+                message: 'user deletion successful',
+                payload: {},
+              })
+            }
+            reject({
+              error: true,
+              message: 'username not found',
+              payload: 'username not found',
+            })
+          })
+          .catch((err) => {
+            logger.error('error in deleting user')
+            reject({
+              error: true,
+              message: 'unexpected error in deleting user',
+              payload: err,
+            })
+          })
+      } catch (error) {
+        reject({
+          error: true,
+          message: 'unexpected error in deleting user',
+          payload: error,
+        })
+      }
     })
   }
 
-  static getUser(req: Express.Request) {
+  static getUser(req: Request) {
     return new Promise<SuccessResponse>((resolve, reject) => {
-      resolve({
-        message: 'some fancy resolve message',
-        error: false,
-        payload: 'some data',
-      })
+      if (!req.body.username) {
+        reject({
+          error: true,
+          message: 'username required to return details',
+          payload: {},
+        })
+      }
+
+      UserSchema.findOne({ username: req.body.username })
+        .then((user) => {
+          if (!user) {
+            reject({
+              error: true,
+              message: 'user not found',
+              payload: {},
+            })
+          }
+          resolve({
+            error: false,
+            message: 'user details found',
+            payload: user,
+          })
+        })
+        .catch((err) => {
+          logger.error('error getting user data')
+          reject({
+            error: true,
+            message: 'unexpected error fetching user data',
+            payload: err,
+          })
+        })
     })
   }
 
-  static update(req: Express.Request) {
+  static update(req: Request) {
     return new Promise<SuccessResponse>((resolve, reject) => {
-      resolve({
-        message: 'some fancy resolve message',
-        error: false,
-        payload: 'some data',
-      })
+      // check if username to be updated actually exists
+      if (!req.body.user || !req.body.user._id) {
+        reject({
+          error: true,
+          message: 'user_id needed to update document',
+          payload: {},
+        })
+      }
+
+      UserSchema.findByIdAndUpdate(req.body.user._id, { $set: req.body.user })
+        .exec()
+        .then((resp) => {
+          resolve({
+            error: false,
+            message: 'user update successful',
+            payload: resp,
+          })
+        })
+        .catch((err) => {
+          logger.error('error updating user')
+          reject({
+            error: true,
+            message: 'error updating user by id',
+            payload: err,
+          })
+        })
     })
   }
 }
