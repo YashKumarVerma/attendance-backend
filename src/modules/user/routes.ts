4295d2f410ef965970968618324be2c9835d5e30
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-05-02 02:17:21 +0530

    Transforming Connections

--- src/modules/user/routes.ts
+++ src/modules/user/routes.ts
@@ -1,68 +1,82 @@
 import express, { Request, Response } from 'express'
 
 import UserOperations from './controller'
-import { SuccessResponse } from './interface'
 
 const router = express.Router()
 
-router.post('/create', (req: Request, res: Response): void => {
-  UserOperations.add(req)
-    .then((resp: SuccessResponse) => {
-      res.status(200).json(resp)
+router.post('/create', async (req: Request, res: Response) => {
+  try {
+    const response = await UserOperations.createNewUser(req)
+    res.status(response.code).json({
+      error: false,
+      message: response.message,
+      payload: response.payload,
     })
-    .catch((error) => {
-      res.status(400).json(error)
+  } catch (e) {
+    res.status(e.code).json({
+      error: true,
+      message: e.message,
+      payload: e.payload,
     })
+  }
 })
 
-router.delete('/:username', (req: Request, res: Response): void => {
-  UserOperations.delete(req)
-    .then((resp: SuccessResponse) => {
-      res.json(resp)
-    })
-    .catch((error: any) => {
-      res.json(error)
-    })
-})
+//   UserOperations.add(req)
+//     .then((resp: SuccessResponse) => {
+//       res.status(200).json(resp)
+//     })
+//     .catch((error) => {
+//       res.status(400).json(error)
+//     })
 
-router.get('/:username', (req: Request, res: Response): void => {
-  UserOperations.getUser(req)
-    .then((resp: SuccessResponse) => {
-      res.json(resp) // property send does not exist in response
-    })
-    .catch((error: any) => {
-      res.json(error)
-    })
-})
+// router.delete('/:username', (req: Request, res: Response): void => {
+//   UserOperations.delete(req)
+//     .then((resp: SuccessResponse) => {
+//       res.json(resp)
+//     })
+//     .catch((error: any) => {
+//       res.json(error)
+//     })
+// })
 
-router.put('/update', (req: Request, res: Response): void => {
-  UserOperations.update(req)
-    .then((resp: SuccessResponse) => {
-      res.json(resp)
-    })
-    .catch((error: any) => {
-      res.json(error)
-    })
-})
+// router.get('/:username', (req: Request, res: Response): void => {
+//   UserOperations.getUser(req)
+//     .then((resp: SuccessResponse) => {
+//       res.json(resp) // property send does not exist in response
+//     })
+//     .catch((error: any) => {
+//       res.json(error)
+//     })
+// })
 
-router.get('/:username/:eventSlug', (req: Request, res: Response): void => {
-  UserOperations.getEvents(req)
-    .then((resp: SuccessResponse) => {
-      res.json(resp)
-    })
-    .catch((error: any) => {
-      res.json(error)
-    })
-})
+// router.put('/update', (req: Request, res: Response): void => {
+//   UserOperations.update(req)
+//     .then((resp: SuccessResponse) => {
+//       res.json(resp)
+//     })
+//     .catch((error: any) => {
+//       res.json(error)
+//     })
+// })
 
-router.post('/login', (req: Request, res: Response): void => {
-  UserOperations.login(req)
-    .then((resp: SuccessResponse) => {
-      res.status(200).json(resp)
-    })
-    .catch((error) => {
-      res.status(401).json(error)
-    })
-})
+// router.get('/:username/:eventSlug', (req: Request, res: Response): void => {
+//   UserOperations.getEvents(req)
+//     .then((resp: SuccessResponse) => {
+//       res.json(resp)
+//     })
+//     .catch((error: any) => {
+//       res.json(error)
+//     })
+// })
+
+// router.post('/login', (req: Request, res: Response): void => {
+//   UserOperations.login(req)
+//     .then((resp: SuccessResponse) => {
+//       res.status(200).json(resp)
+//     })
+//     .catch((error) => {
+//       res.status(401).json(error)
+//     })
+// })
 
 export default router
