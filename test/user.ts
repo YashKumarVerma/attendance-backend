dc71280e45ccbc835f8f6b72a2c7abf452f8f436
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-05-13 21:20:37 +0530

    Testing incomplete requests

--- test/user.ts
+++ test/user.ts
@@ -32,6 +32,23 @@ describe(' => Testing /user route', () => {
 
   const url = 'http://localhost:3000/user'
 
+  //   testing /create
+  it('should throw error creating user with incomplete request', (done) => {
+    fetch(`${url}/create`, {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((res) => res.json())
+      .then((resp) => {
+        expect(resp.error).to.be.true
+        console.log(resp)
+        done()
+      })
+      .catch((err) => {
+        done(err)
+      })
+  })
+
   it('should create a new user', (done) => {
     fetch(`${url}/create`, {
       method: 'post',
@@ -101,6 +118,23 @@ describe(' => Testing /user route', () => {
       })
   })
 
+  //   testing /login
+  it('should throw error creating user with incomplete request', (done) => {
+    fetch(`${url}/login`, {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((res) => res.json())
+      .then((resp) => {
+        expect(resp.error).to.be.true
+        console.log(resp)
+        done()
+      })
+      .catch((err) => {
+        done(err)
+      })
+  })
+
   it('should successfully login and return a jwt token', (done) => {
     fetch(`${url}/login`, {
       method: 'post',
@@ -127,6 +161,25 @@ describe(' => Testing /user route', () => {
       body: JSON.stringify({
         username: 'yashkumarverma',
         password: 'wront password',
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((res) => res.json())
+      .then((resp) => {
+        expect(resp).to.have.all.keys('error', 'message', 'payload')
+        expect(resp.error).to.be.true
+        done()
+      })
+      .catch((err) => {
+        done(err)
+      })
+  })
+
+  it('should not login because of incomplete request', (done) => {
+    fetch(`${url}/login`, {
+      method: 'post',
+      body: JSON.stringify({
+        username: 'yashkumarverma',
       }),
       headers: { 'Content-Type': 'application/json' },
     })
