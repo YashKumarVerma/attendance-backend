72d7a5df55e318c39d8dd5b8d9a75984f2300720
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-05-13 22:06:11 +0530

    dummy test to just start server

---
+++ test/arrange.ts
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-expressions */
+import { describe, before, it } from 'mocha'
+import serverInstance from '../src/app'
+
+import logger from '../src/modules/logger/winston'
+import Seed from '../src/modules/database/validator'
+import { db, connect } from '../src/modules/database/mongo'
+
+describe(' => Testing / route', () => {
+  before((done) => {
+    try {
+      serverInstance.listen(3001)
+      setTimeout(() => {
+        connect(async () => {
+          logger.info('Connecting to database to clear old records')
+          Seed()
+          db.collection('users').deleteMany({})
+          logger.info('Cleared collection : users')
+          db.collection('events').deleteMany({})
+          logger.info('Cleared collection : events')
+          db.collection('sessions').deleteMany({})
+          logger.info('Cleared collection : sessions')
+          done()
+        })
+      }, 3000)
+    } catch (error) {
+      done(error)
+    }
+  })
+
+  //   testing connection
+  it('should just pass', (done) => {
+    done()
+  })
+})
